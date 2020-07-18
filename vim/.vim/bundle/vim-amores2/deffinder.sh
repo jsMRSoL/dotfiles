5cfe146d00563f1b4b96acbcd27bc0c63a8c8b43
jsMRSoL <si_pitt@yahoo.com>
2020-07-18 13:04:20 +0100

    Revert "Add all needed dwm files"
    This reverts commit 8f5b76024e6c53610cc6f9a529b3047920dde621.

--- vim/.vim/bundle/vim-amores2/deffinder.sh
+++ vim/.vim/bundle/vim-amores2/deffinder.sh
@@ -1,1 +1,7 @@
-../../../../.dotfiles/vim/.config/vim/bundle/vim-amores2/deffinder.sh
+#!/bin/bash
+word="$1"
+dictionary="/home/simon/.config/vim/bundle/vim-amores2/amores2vocab.txt"
+
+awk -F ': ' '/^'"$1[^a-z]"'/ {print $2}' "$dictionary"
+
+
