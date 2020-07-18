8f5b76024e6c53610cc6f9a529b3047920dde621
jsMRSoL <si_pitt@yahoo.com>
2020-07-18 12:20:12 +0100

    Add all needed dwm files

--- vim/.vim/bundle/vim-amores2/deffinder.sh
+++ vim/.vim/bundle/vim-amores2/deffinder.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-word="$1"
-dictionary="/home/simon/.config/vim/bundle/vim-amores2/amores2vocab.txt"
-
-awk -F ': ' '/^'"$1[^a-z]"'/ {print $2}' "$dictionary"
-
-
+../../../../.dotfiles/vim/.config/vim/bundle/vim-amores2/deffinder.sh
