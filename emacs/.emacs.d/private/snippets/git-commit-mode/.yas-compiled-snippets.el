5d02c88c6b61eebe14a47f036f615b2e764b26c3
Simon Pitt <si_pitt@yahoo.com>
2021-07-24 16:04:01 +0100

    Add emacs snippets.

---
+++ emacs/.emacs.d/private/snippets/git-commit-mode/.yas-compiled-snippets.el
@@ -0,0 +1,10 @@
+;;; Compiled snippets and support files for `git-commit-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'git-commit-mode
+		     '(("type" "${1:$$(yas-choose-value '(\"fix\" \"hotfix\" \"add\" \"update\" \"change\" \"clean\" \"disable\" \"remove\" \"upgrade\" \"revert\"))} $0" "type" nil nil nil "/home/simon/.dotfiles/emacs/.emacs.d/private/snippets/git-commit-mode/type" nil nil)
+		       ("ref" "references #${1:100}" "references" nil nil nil "/home/simon/.dotfiles/emacs/.emacs.d/private/snippets/git-commit-mode/references" nil nil)
+		       ("fix" "fixes #${1:100}" "fixes" nil nil nil "/home/simon/.dotfiles/emacs/.emacs.d/private/snippets/git-commit-mode/fixes" nil nil)))
+
+
+;;; Do not edit! File generated at Thu Jul 22 14:01:54 2021
---
+++ emacs/.emacs.d/private/snippets/js2-mode/.yas-compiled-snippets.el
@@ -0,0 +1,2 @@
+;;; Compiled snippets and support files for `js2-mode'
+;;; Do not edit! File generated at Thu Jul 22 14:01:55 2021
