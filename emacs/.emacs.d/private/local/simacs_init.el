c732a57af877ec26183c16d2650282808d972beb
Simon Pitt <si_pitt@yahoo.com>
2021-07-24 19:33:31 +0100

    Minor changes.

--- emacs/.emacs.d/private/local/simacs_init.el
+++ emacs/.emacs.d/private/local/simacs_init.el
@@ -36,9 +36,12 @@
 ;; disable line numbers for these modes:
 (dolist (mode '(org-mode-hook
                  term-mode-hook
+		 treemacs-mode-hook
                  shell-mode-hook
                  eshell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+(defalias 'yes-or-no-p 'y-or-n-p)
 
 (use-package ivy
     :diminish
@@ -106,6 +109,31 @@
   :config
   (setq-default evil-escape-key-sequence "fd"))
 
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
+(use-package dired
+  :ensure nil
+  :commands (dired dired-jump)
+  :custom ((dired-listing-switches "-agho --group-directories-first"))
+  :config
+  (evil-collection-define-key 'normal 'dired-mode-map
+    "h" 'dired-single-up-directory
+    "l" 'dired-single-buffer))
+
+(use-package dired-single)
+
+(use-package all-the-icons-dired
+  :hook (dired-mode . all-the-icons-dired-mode))
+
+(use-package dired-hide-dotfiles
+  :hook (dired-mode . dired-hide-dotfiles-mode)
+  :config
+  (evil-collection-define-key 'normal 'dired-mode-map
+    "H" 'dired-hide-dotfiles-mode))
+
 (use-package general
   :config
   (general-evil-setup t)
@@ -115,13 +143,25 @@
     :prefix "SPC"
     :global-prefix "C-SPC"))
 
+(defun sp/open-init ()
+  "Open init.el for simacs."
+  (interactive)
+  (find-file "~/.simacs_dir/init.el"))
+
 (sp/leader-keys
  "1" '(winum-select-window-1 :which-key "win 1")
  "2" '(winum-select-window-2 :which-key "win 2")
+ "a" '(:ignore t :which-key "apps")
+ "at" '(vterm :which-key "terminal")
+ "au" '(undo-tree-visualize :which-key "undo-tree")
  "f" '(:ignore t :which-key "files")
+ "fed" '(sp/open-init :which-key "edit init.el")
  "ff" '(counsel-find-file :which-key "find file")
  "fr" '(counsel-recentf :which-key "find recent")
  "fs" '(save-buffer :which-key "save")
+ "ft" '(treemacs :which-key "treemacs")
+ "g" '(:ignore t :which-key "git")
+ "gs" '(magit-status :which-key "status")
  "b" '(:ignore t :which-key "buffers")
  "bb" '(counsel-switch-buffer :which-key "switch")
  "bd" '(kill-buffer-and-window :which-key "delete")
@@ -137,8 +177,9 @@
  "cc" '(comment-line :which-key "comment")
  "r" '(:ignore t :which-key "registers")
  "rl" '(evil-show-registers :which-key "list")
- "p" '(:ignore t :which-key "projects")
- "pf" '(projectile-find-file :which-key "find")
+ "p" '(projectile-command-map :which-key "projects")
+ ;; "p" '(:ignore t :which-key "projects")
+ ;; "pf" '(projectile-find-file :which-key "find")
  "s" '(:ignore t :which-key "search")
  "sp" '(swiper :which-key "swiper")
  "ss" '(avy-goto-char-2 :which-key "char2")
@@ -168,9 +209,13 @@
 			  )))
 
 (use-package projectile
-  :defer
+  :diminish projectile-mode
   :config
-  (projectile-mode +1))
+  (projectile-mode +1)
+  :custom ((projectile-completion-system 'ivy)))
+
+(use-package counsel-projectile
+  :config (counsel-projectile-mode))
 
 (use-package evil-snipe
   :defer
@@ -183,9 +228,6 @@
   (evil-snipe-override-mode +1))
 
 (use-package avy)
-
-;;(require 'smartparens-config)
-;;(smartparens-global-mode 1)
 
 (use-package hydra)
 
@@ -223,6 +265,10 @@
 	(call-interactively 'winum-select-window-by-number)
 	))))
 
+(use-package undo-tree
+  :init
+  (global-undo-tree-mode))
+
 (use-package company
   :defer
   :bind (
@@ -232,16 +278,105 @@
   :init
   (global-company-mode 1))
 
+(use-package company-box
+  :hook (company-mode . company-box-mode))
+
+(use-package vterm
+  :commands vterm)
+
+(use-package magit
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+(use-package flycheck)
+
 (use-package lsp-mode
   :commands (lsp lsp-deferred)
+  :init 
+  (setq lsp-keymap-prefix "C-c l")
   :config
   (lsp-enable-which-key-integration t))
 
-(use-package python-mode)
+(use-package lsp-ui
+  :hook (lsp-mode . lsp-ui-mode)
+  :custom
+  (lsp-ui-doc-position 'bottom))
 
-(use-package gnu-elpa-keyring-update)
+(use-package lsp-treemacs
+  :after lsp)
 
-;; (setq initial-buffer-choice (lambda () (get-buffer "*dashboard*")))
+(use-package lsp-ivy)
+
+(use-package pyvenv
+  :defer)
+
+(use-package lsp-pyright
+  :defer)
+
+(defun sp/setup-python-lsp ()
+  (require 'pyvenv)
+  (pyvenv-mode 1)
+  (require 'lsp-pyright)
+  ;; (fset 'lsp-format-buffer 'yapfify-buffer)
+  ;; (fset 'lsp-format-region 'yapfify-region)
+  (lsp-deferred) ;; or lsp
+  (require 'yapfify)
+  (general-define-key
+    :keymaps 'lsp-mode-map
+    :prefix lsp-keymap-prefix
+    "= =" '(yapfify-buffer :which-key "format buffer")
+    "= r" '(yapfify-region-or-buffer :which-key "format region")))
+
+(use-package python-mode
+  :mode "\\.py\\'"
+  :hook
+  (python-mode . sp/setup-python-lsp)
+  :custom
+  (python-shell-interpreter "python")
+  (dap-python-executable "python")
+  (dap-python-debugger 'debugpy)
+  :config
+  (require 'dap-python))
+
+(defun sp/setup-rust-lsp ()
+  (lsp-deferred))
+
+(use-package rustic
+  :hook
+  (rustic-mode . sp/setup-rust-lsp))
+
+(use-package dap-mode
+  ;; Uncomment the config below if you want all UI panes to be hidden by default!
+  ;; :custom
+  ;; (lsp-enable-dap-auto-configure nil)
+  ;; :config
+  ;; (dap-ui-mode 1)
+
+  :config
+  ;; Set up Node debugging
+  (require 'dap-node)
+  (dap-node-setup) ;; Automatically installs Node debug adapter if needed
+
+  ;; Bind `C-c l d` to `dap-hydra` for easy access
+  (general-define-key
+    :keymaps 'lsp-mode-map
+    :prefix lsp-keymap-prefix
+    "d" '(dap-hydra t :which-key "debugger")))
+
+(use-package ob-rust)
+(org-babel-do-load-languages
+  'org-babel-load-languages
+  '((emacs-lisp . t)
+    (python . t)
+    (rust . t)))
+
+(require 'org-tempo)
+(add-to-list 'org-structure-template-alist '("sh" . "src shell"))
+(add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+(add-to-list 'org-structure-template-alist '("py" . "src python"))
+(add-to-list 'org-structure-template-alist '("rs" . "src rust"))
+
+
 
 ;; Don't edit below here.
 (custom-set-variables
@@ -250,7 +385,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(spinner gnu-elpa-keyring-update lsp-mode python-mode winum which-key use-package rainbow-delimiters projectile page-break-lines ivy-rich hydra general evil-snipe evil-escape doom-themes doom-modeline dashboard counsel company avy)))
+   '(ob-rust org-babel rustic flycheck undo-tree evil-magit magit pyvenv dap-mode yapfify lsp-pyright counsel-projectile dired-hide-dotfiles all-the-icons-dired dired-single dired vterm lsp-ivy lsp-treemacs evil-collection lsp-ui company-box spinner gnu-elpa-keyring-update lsp-mode python-mode winum which-key use-package rainbow-delimiters projectile page-break-lines ivy-rich hydra general evil-snipe evil-escape doom-themes doom-modeline dashboard counsel company avy)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
