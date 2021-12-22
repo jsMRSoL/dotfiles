dc97e9981b8599af6bf03e94f46469fa99725bd7
Simon Pitt <si_pitt@yahoo.com>
2021-12-22 23:20:02 +0000

    Add cmp-path and cmp-cmdline.

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -37,6 +37,8 @@ return require("packer").startup(function()
   -- autocomplete
   use("hrsh7th/cmp-nvim-lsp")
   use("hrsh7th/cmp-buffer")
+  use("hrsh7th/cmp-path")
+  use("hrsh7th/cmp-cmdline")
   use("hrsh7th/nvim-cmp")
   -- For vsnip users.
   use("hrsh7th/cmp-vsnip")
