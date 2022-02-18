4e73924607f312888eed377b458d2634fe81b5a6
Simon Pitt <si_pitt@yahoo.com>
2022-02-18 21:03:57 +0000

    Add early-init.el.
    Adjust gc-cons-threshold in early-init.el and reset in init.el.

---
+++ simacs/.simacs_dir/early-init.el
@@ -0,0 +1,24 @@
+;;; init.el -*- lexical-binding: t; -*-
+
+;; Increase the GC threshold for faster startup
+;; The default is 800 kilobytes.  Measured in bytes.
+(setq gc-cons-threshold (* 50 1000 1000))
+(message "gc-cons-threshold set to 50MB")
+
+;; Don't use package.el, we'll use straight.el instead
+;; (setq package-enable-at-startup t)
+
+;; Remove some unneeded UI elements (the user can turn back on anything they wish)
+(setq inhibit-startup-message t)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+(push '(background-color . "#000000") default-frame-alist)
+(push '(foreground-color . "#FFFFFF") default-frame-alist)
+(push '(mouse-color . "white") default-frame-alist)
+
+;; Make the initial buffer load faster by setting its mode to fundamental-mode
+(setq initial-major-mode 'fundamental-mode)
+;; change to ~100 MB
+(setq large-file-warning-threshold 100000000)
+
