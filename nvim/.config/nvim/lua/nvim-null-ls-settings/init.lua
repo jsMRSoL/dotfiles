2077c7b87fe69717536f4286c85dfa1d8f3a77d4
Simon Pitt <si_pitt@yahoo.com>
2022-06-19 23:50:21 +0100

    nvim: attempt to add mypy type checking for python.

--- nvim/.config/nvim/lua/nvim-null-ls-settings/init.lua
+++ nvim/.config/nvim/lua/nvim-null-ls-settings/init.lua
@@ -2,7 +2,9 @@ require("null-ls").setup({
   sources = {
     require("null-ls").builtins.formatting.stylua,
     require("null-ls").builtins.formatting.yapf,
-    require("null-ls").builtins.diagnostics.mypy,
+    require("null-ls").builtins.diagnostics.mypy.with({
+      extra_args = { "--strict" },
+    }),
     require("null-ls").builtins.formatting.shfmt.with({
       extra_args = { "-i", "2", "-ci", "-kp" },
     }),
