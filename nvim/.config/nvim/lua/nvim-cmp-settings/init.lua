9544b43988b9ab410da5bacd0eb2b0f62df36d7f
Simon Pitt <si_pitt@yahoo.com>
2022-06-21 21:38:31 +0100

    nvim: update nvim-cmp settings. Popup menu now beautiful!

--- nvim/.config/nvim/lua/nvim-cmp-settings/init.lua
+++ nvim/.config/nvim/lua/nvim-cmp-settings/init.lua
@@ -46,13 +46,20 @@ cmp.setup({
       -- require'snippy'.expand_snippet(args.body) -- For `snippy` users.
     end,
   },
-  mapping = {
+  -- mapping = {
+  --   ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+  --   ["<C-f>"] = cmp.mapping.scroll_docs(4),
+  --   ["<C-Space>"] = cmp.mapping.complete(),
+  --   ["<C-e>"] = cmp.mapping.close(),
+  --   ["<CR>"] = cmp.mapping.confirm({ select = true }),
+  -- },
+  mapping = cmp.mapping.preset.insert({
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
-    ["<C-e>"] = cmp.mapping.close(),
-    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-  },
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
   formatting = {
     fields = { "kind", "abbr", "menu" },
     format = function(entry, vim_item)
@@ -81,8 +88,12 @@ cmp.setup({
   -- documentation = {
   --   border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
   -- },
+  -- window = {
+  --   documentation = "native"
+  -- },
   window = {
-    documentation = "native"
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
   },
   experimental = {
     ghost_text = false,
@@ -91,14 +102,28 @@ cmp.setup({
 })
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+-- cmp.setup.cmdline("/", {
+--   sources = {
+--     { name = "buffer" },
+--   },
+-- })
 cmp.setup.cmdline("/", {
+  mapping = cmp.mapping.preset.cmdline(),
   sources = {
     { name = "buffer" },
   },
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+-- cmp.setup.cmdline(":", {
+--   sources = cmp.config.sources({
+--     { name = "path" },
+--   }, {
+--     { name = "cmdline" },
+--   }),
+-- })
 cmp.setup.cmdline(":", {
+  mapping = cmp.mapping.preset.cmdline(),
   sources = cmp.config.sources({
     { name = "path" },
   }, {
