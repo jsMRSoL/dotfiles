50761cc94f120af10cc43b7e83d4eb637b06a990
Simon Pitt <si_pitt@yahoo.com>
2023-06-04 23:48:57 +0100

    Shell: Fix bookmarklauncher.sh and start new bookmarks file.

--- scripts/.local/usr/bin/bookmarklauncher.sh
+++ scripts/.local/usr/bin/bookmarklauncher.sh
@@ -2,12 +2,15 @@
 file="/home/simon/.bookmarks"
 [ ! -e $file ] && notify-send "Cannot find bookmarks file" && exit
 choice=$( awk '{print $1}' $file | dmenu -l 10 -p "Choose bookmark: ")
+# echo "Choice was: $choice"
 [ -z "$choice" ] && exit
 search='/^'"$choice"'/{$1 = ""; print $0}'
-cmd=$( awk "$search" "$file" )
+# echo "Searching pattern is: $search"
+target=$( awk "$search" "$file" )
+# echo "Site is: $target"
 # printf "%s" "$choice"
-[ -z "$cmd" ] && cmd=$(printf "waterfox \"https://duckduckgo.com/?q=%s\"" "$choice")
+BROWSER="firefox"
+[ -z "$target" ] && cmd=$(printf "%s \"https://duckduckgo.com/?q=%s\"" "$BROWSER" "$choice")
+[ -n "$target" ] && cmd=$(printf "%s %s" "$BROWSER" "$target")
 # echo "$cmd"
 /bin/bash -c "$cmd" & disown
-
-
