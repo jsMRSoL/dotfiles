74ed5928aae535a345a808f793a219ed885b7564
Simon Pitt <si_pitt@yahoo.com>
2023-06-11 15:59:17 +0100

    MyVim: Add bash debug adapter dap config.

--- MyVim/.config/MyVim/lua/plugins/dap.lua
+++ MyVim/.config/MyVim/lua/plugins/dap.lua
@@ -1,50 +1,79 @@
 return {
   {
-    "mfussenegger/nvim-dap",
+    'mfussenegger/nvim-dap',
     config = function()
-      local dap = require("dap")
-      local dapui = require("dapui")
-      dap.listeners.after.event_initialized["dapui_config"] = function()
+      local dap = require('dap')
+      local dapui = require('dapui')
+      dap.listeners.after.event_initialized['dapui_config'] = function()
         dapui.open()
       end
-      dap.listeners.after.event_terminated["dapui_config"] = function()
+      dap.listeners.after.event_terminated['dapui_config'] = function()
         dapui.close()
       end
-      dap.listeners.after.event_exited["dapui_config"] = function()
+      dap.listeners.after.event_exited['dapui_config'] = function()
         dapui.close()
       end
 
       local signs = {
-        ["DapBreakpoint"] = { text = " ", texthl = "", linehl = "", numhl = "" },
-        ["DapLogPoint"] = { text = " ", texthl = "", linehl = "", numhl = "" },
-        ["DapStopped"] = { text = " ", texthl = "", linehl = "", numhl = "" },
-        ["DapBreakpointCondition"] = { text = " ", texthl = "", linehl = "", numhl = "" },
-        ["DapBreakpointRejected"] = { text = " ", texthl = "", linehl = "", numhl = "" },
+        ['DapBreakpoint'] = { text = ' ', texthl = '', linehl = '', numhl = '' },
+        ['DapLogPoint'] = { text = ' ', texthl = '', linehl = '', numhl = '' },
+        ['DapStopped'] = { text = ' ', texthl = '', linehl = '', numhl = '' },
+        ['DapBreakpointCondition'] = { text = ' ', texthl = '', linehl = '', numhl = '' },
+        ['DapBreakpointRejected'] = { text = ' ', texthl = '', linehl = '', numhl = '' },
       }
       for k, v in pairs(signs) do
         vim.fn.sign_define(k, v)
       end
 
-    end
+      dap.adapters.bashdb = {
+        type = 'executable',
+        command = vim.fn.stdpath('data') .. '/mason/packages/bash-debug-adapter/bash-debug-adapter',
+        name = 'bashdb',
+      }
+
+      dap.configurations.sh = {
+       {
+          type = 'bashdb',
+          request = 'launch',
+          name = 'Launch file',
+          showDebugOutput = true,
+          pathBashdb = vim.fn.stdpath('data') .. '/mason/packages/bash-debug-adapter/extension/bashdb_dir/bashdb',
+          pathBashdbLib = vim.fn.stdpath('data') .. '/mason/packages/bash-debug-adapter/extension/bashdb_dir',
+          trace = true,
+          file = '${file}',
+          program = '${file}',
+          cwd = '${workspaceFolder}',
+          pathCat = 'cat',
+          pathBash = '/bin/bash',
+          pathMkfifo = 'mkfifo',
+          pathPkill = 'pkill',
+          args = { '--debugger' },
+          env = {},
+          terminalKind = 'integrated',
+        },
+      }
+    end,
   },
   {
-    "rcarriga/nvim-dap-ui",
-    dependencies = { "mfussenegger/nvim-dap" },
-    config = true
+    'rcarriga/nvim-dap-ui',
+    dependencies = { 'mfussenegger/nvim-dap' },
+    config = true,
   },
-  { 'jbyuki/one-small-step-for-vimkind',
-    config = function ()
+  {
+    'jbyuki/one-small-step-for-vimkind',
+    config = function()
       local dap = require('dap')
       dap.configurations.lua = {
         {
           type = 'nlua',
           request = 'attach',
-          name = "Attach to running Neovim instance",
-        }
+          name = 'Attach to running Neovim instance',
+        },
       }
 
       dap.adapters.nlua = function(callback, config)
-        callback({ type = 'server', host = config.host or "127.0.0.1", port = config.port or 8086 })
+        callback({ type = 'server', host = config.host or '127.0.0.1', port = config.port or 8086 })
       end
-    end},
+    end,
+  },
 }
