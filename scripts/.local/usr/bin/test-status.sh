f031b485fdc0cee5a86e20ac6bb2df75b129c8ea
Simon Pitt <si_pitt@yahoo.com>
2023-06-11 01:07:02 +0100

    Formatting changes.

--- scripts/.local/usr/bin/test-status.sh
+++ scripts/.local/usr/bin/test-status.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-
 INT="$(get-internet-connectivity)"
 VOL="$(get-system-volume)"
-IP=$(for i in `ip r`; do echo $i; done | grep -A 1 src | tail -n1) # can get confused if you use vmware
+IP=$(for i in $(ip r); do echo $i; done | grep -A 1 src | tail -n1) # can get confused if you use vmware
 TEMP="$(get-system-temperature)"
 LOCALTIME="$(get-local-time)"
 BAT="$(get-battery)"
 
 echo " $INT $IP | $BAT | $VOL | $TEMP | $LOCALTIME"
-
-
 
 # 192.168.1.114
