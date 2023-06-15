267bfd0da1e8d584f4cbcc62c5602102c73be704
Simon Pitt <si_pitt@yahoo.com>
2023-06-15 23:26:39 +0100

    Update dmenu options.

--- scripts/.local/usr/bin/webview.sh
+++ scripts/.local/usr/bin/webview.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
 
-webcli() { w3m "$1" ;}
-webgui() { waterfox "$1" 2&>/dev/null & disown ;}
-vidplay() { mpv --no-terminal "$1" 2&>/dev/null & disown ;}
-gifview() { sxiv -a -s f -- "$1" 2&>/dev/null & disown ;}
-pdfview() { mupdf "$1" 2&>/dev/null & disown ;}
-filedl() { wget "$1" 2&>/dev/null & disown ;}
-imageview() { feh -. "$1" & disown ;}
-ytdl() { youtube-viewer "$1" & disown ;}
-saveplay() { ytsp "$1" ;}
+webcli() { w3m "$1"; }
+webgui() { firefox "$1" 2 &> /dev/null & disown; }
+vidplay() { mpv --no-terminal "$1" 2 &> /dev/null & disown; }
+gifview() { sxiv -a -s f -- "$1" 2 &> /dev/null & disown; }
+pdfview() { mupdf "$1" 2 &> /dev/null & disown; }
+filedl() { wget "$1" 2 &> /dev/null & disown; }
+imageview() { feh -. "$1" & disown; }
+ytdl() { youtube-viewer "$1" & disown; }
+saveplay() { ytsp "$1"; }
 
 # dmenu function
 # The URL will be shown visually in 30 characters or less.
 ask() {
-x=$(echo -e "clip\nw3m\nwaterfox\nmpv\nmupdf\nwget\nsaveplay\nsxiv\nytdl" | dmenu -p "How to open?")
+  dmenu_opts=(-l 9 -fn 'FiraCode Nerd Font-15' -nf '#cdd6f4' -nb '#1e1e2e' -sf '#D8DEE9' -sb '#626880' -p 'Choose application: ')
+  x=$(echo -e "clip\nw3m\nfirefox\nmpv\nmupdf\nwget\nsaveplay\nsxiv\nytdl" | dmenu "${dmenu_opts[@]}")
 
-case "$x" in
+  case "$x" in
     clip) printf '%s' "$1" | xclip -selection clipboard ;;
-	w3m) webcli "$1" ;;
-	waterfox) webgui "$1" ;;
-	mpv) vidplay "$1" ;;
-	feh) imageview "$1" ;;
-	mupdf) pdfview "$1" ;;
-	wget) filedl "$1" ;;
-	sxiv) gifview "$1" ;;
-	ytdl) ytdl "$1" ;;
+    w3m) webcli "$1" ;;
+    firefox) webgui "$1" ;;
+    mpv) vidplay "$1" ;;
+    feh) imageview "$1" ;;
+    mupdf) pdfview "$1" ;;
+    wget) filedl "$1" ;;
+    sxiv) gifview "$1" ;;
+    ytdl) ytdl "$1" ;;
     saveplay) ytsp "$1" ;;
-esac
+  esac
 }
 
 # Get the extension and try to decide what to do.
@@ -51,6 +52,6 @@ esac
 # elif [[ "$1" == *"youtu"* ]]; then
 # 	ytdl "$1"
 # else
-# 	ask "$1" 
+# 	ask "$1"
 # fi
 ask "$1"
