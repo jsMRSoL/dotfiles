cf823bcb362dd1cb76b824b86650ebf6c32bdd66
Simon Pitt <si_pitt@yahoo.com>
2024-05-17 22:09:12 +0100

    First commit of updatesystem.sh

--- scripts/.local/usr/bin/updatesystem.sh
+++ scripts/.local/usr/bin/updatesystem.sh
@@ -1,10 +1,53 @@
 #!/bin/bash
 
-# A wrapper script to handle updating the system.
+# A simple wrapper script to handle updating the system.
 
 # run pacman
+clear
+printf "\n~~~~~~~~~~~~~~~~~SYSTEM UPDATE: 1/5~~~~~~~~~~~~~~~~~\n"
+echo "Check packages? [Y/n]"
+read -r packages
+if [ "$packages" != "n" ]; then
+  sudo pacman -Syu
+fi
+clear
+
 # run paru
-# run checkservices
-# remove unnecessary packages
+printf "\n~~~~~~~~~~~~~~~~~SYSTEM UPDATE: 2/5~~~~~~~~~~~~~~~~~\n"
+echo "Check aur packages? [Y/n]"
+read -r aur_packages
+if [ "$aur_packages" != "n" ]; then
+  sudo paru -Syu
+fi
+clear
+
+# run checkservice
+printf "\n~~~~~~~~~~~~~~~~~SYSTEM UPDATE: 3/5~~~~~~~~~~~~~~~~~\n"
+echo "Run checkservices? [Y/n]"
+read -r check_services
+if [ "$check_services" != "n" ]; then
+  sudo checkservices
+fi
+clear
+
 # remove orphans
-# clear out repo files
+printf "\n~~~~~~~~~~~~~~~~~SYSTEM UPDATE: 4/5~~~~~~~~~~~~~~~~~\n"
+echo "Remove orphans? [y/N] "
+read -r orphans
+if [ "$orphans" == "y" ]; then
+  sudo pacman -Qdtq | sudo pacman -Rns -
+fi
+clear
+
+# clear paccache
+printf "\n~~~~~~~~~~~~~~~~~SYSTEM UPDATE: 5/5~~~~~~~~~~~~~~~~~\n"
+echo "Clear package caches? [y/N] "
+read -r pac_cache
+if [ "$pac_cache" == "y" ]; then
+  sudo paru -Sccd
+fi
+
+printf "\n~~~~~~~~~~~~~~SYSTEM UPDATE: FINISHED~~~~~~~~~~~~~~~\n"
+killall -SIGUSR2 waybar
+sleep 2
+clear
