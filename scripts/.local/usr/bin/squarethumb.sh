e863921926d640cb58f289b91bc6b3810486bc0a
Simon Pitt <si_pitt@yahoo.com>
2024-05-17 22:08:13 +0100

    Change fit algo. Use ffmpegthumbnailer for gif.

--- scripts/.local/usr/bin/squarethumb.sh
+++ scripts/.local/usr/bin/squarethumb.sh
@@ -13,60 +13,69 @@ squarethumb.sh file1.jpg file2.png file3.mp4
 [[ -d thumbs ]] || mkdir thumbs
 
 for var in "$@"; do
-    ext="${var##*.}"
-    # echo "$var"
-    # echo "$ext"
+  ext="${var##*.}"
 
-    case $ext in
-         gif | mp4 )
-            convert $var[0] \
-                    -resize 200 \
-                    -crop 250x250 \
-                    -gravity center \
-                    -extent 250x250 \
-                    thumbs/$var-thumb.jpg 2>/dev/null;;
-        jpg | jpeg | png )
-            convert $var \
-                    -resize 200 \
-                    -crop 250x250 \
-                    -gravity center \
-                    -extent 250x250 \
-                    thumbs/$var-thumb.jpg 2>/dev/null;;
-        webp )
-            if [[ $(grep -c 'ANMF' $var) -gt 0 ]]
-            then
-                webpmux -get frame 10 $var -o thumbs/$var-del.webp
-            else
-                cp $var thumbs/$var-del.webp
-            fi
+  case $ext in
+    gif)
+      convert "$var"[5] \
+        -thumbnail @62500 \
+        -gravity center \
+        -extent 250x250 \
+        thumbs/"$var"-thumb.jpg 2> /dev/null
+      ;;
+    mp4 | MP4 | mov | MOV | mkv)
+      ffmpegthumbnailer -i "$var" \
+        -s 300 \
+        -o thumbs/"$var"-del.jpg 2> /dev/null
+      convert thumbs/"$var"-del.jpg \
+        -thumbnail @62500 \
+        -gravity center \
+        -extent 250x250 \
+        thumbs/"$var"-thumb.jpg 2> /dev/null
+      rm thumbs/"$var"-del.jpg 2> /dev/null
+      ;;
+    jpg | jpeg | JPG | png)
+      convert "$var" \
+        -thumbnail @62500 \
+        -gravity center \
+        -extent 250x250 \
+        thumbs/"$var"-thumb.jpg 2> /dev/null
+      ;;
+    webp)
+      if [[ $(grep -c 'ANMF' "$var") -gt 0 ]]; then
+        webpmux -get frame 10 "$var" -o thumbs/"$var"-del.webp
+      else
+        cp "$var" thumbs/"$var"-del.webp
+      fi
 
-            convert thumbs/$var-del.webp \
-                -resize 200 \
-                -crop 250x250 \
-                -gravity center \
-                -extent 250x250 \
-                thumbs/$var-thumb.jpg 2>/dev/null
-            rm thumbs/$var-del.webp 2>/dev/null;;
-        webm )
-            ffmpegthumbnailer -i $var \
-                -s 250 \
-                -o thumbs/$var-del.jpg 2>/dev/null
-            convert thumbs/$var-del.jpg \
-                -crop 250x250 \
-                -gravity center \
-                -extent 250x250 \
-                thumbs/$var-thumb.jpg 2>/dev/null
-            rm thumbs/$var-del.jpg 2>/dev/null ;;
-     esac
+      convert thumbs/"$var"-del.webp \
+        -thumbnail @62500 \
+        -gravity center \
+        -extent 250x250 \
+        thumbs/"$var"-thumb.jpg 2> /dev/null
+      rm thumbs/"$var"-del.webp 2> /dev/null
+      ;;
+    webm)
+      ffmpegthumbnailer -i "$var" \
+        -s 300 \
+        -o thumbs/"$var"-del.jpg 2> /dev/null
+      convert thumbs/"$var"-del.jpg \
+        -thumbnail @62500 \
+        -gravity center \
+        -extent 250x250 \
+        thumbs/"$var"-thumb.jpg 2> /dev/null
+      rm thumbs/"$var"-del.jpg 2> /dev/null
+      ;;
+  esac
 
 done
 
 # step four : remove unwanted images
 for image in thumbs/*-0.jpg; do
-    mv $image ${image/-0.jpg/.jpg} 2>/dev/null
+  mv "$image" "${image/-0.jpg/.jpg}" 2> /dev/null
 done
 for image in thumbs/*-thumb-*.jpg; do
-    rm $image 2>/dev/null
+  rm "$image" 2> /dev/null
 done
 
 # exit
