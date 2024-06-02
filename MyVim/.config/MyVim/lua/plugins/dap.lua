88058496c3d456bb32f44e2b3f04b51dd60d07df
Simon Pitt <si_pitt@yahoo.com>
2024-06-02 12:30:12 +0100

    MyVim: Add nvim-dap-virtual-text

--- MyVim/.config/MyVim/lua/plugins/dap.lua
+++ MyVim/.config/MyVim/lua/plugins/dap.lua
@@ -2,11 +2,17 @@ return {
   {
     'mfussenegger/nvim-dap',
     event = 'VeryLazy',
-    config = function()
+    dependencies = {
+      {
+        'theHamsta/nvim-dap-virtual-text',
+      }
+    },
+    config = function ()
       local dap = require('dap')
       local dapui = require('dapui')
-      dap.listeners.after.event_initialized['dapui_config'] = function()
-        vim.keymap.set('n', '<right>', '<cmd>DapStepOver<cr>', { desc = 'StepOver' })
+      dap.listeners.after.event_initialized['dapui_config'] = function ()
+        vim.keymap.set('n', '<right>', '<cmd>DapStepOver<cr>',
+          { desc = 'StepOver' })
         vim.keymap.set('n', '<up>', '<cmd>DapStepOut<cr>', { desc = 'StepOut' })
         vim.keymap.set('n', '<down>', '<cmd>DapStepInto<cr>', { desc = 'StepInto' })
         dapui.open()
@@ -14,7 +20,7 @@ return {
       -- dap.listeners.after.event_terminated['dapui_config'] = function()
       --   -- dapui.close()
       -- end
-      dap.listeners.after.event_exited['dapui_config'] = function()
+      dap.listeners.after.event_exited['dapui_config'] = function ()
         vim.keymap.del('n', '<right>')
         vim.keymap.del('n', '<up>')
         vim.keymap.del('n', '<down>')
@@ -34,7 +40,8 @@ return {
 
       dap.adapters.bashdb = {
         type = 'executable',
-        command = vim.fn.stdpath('data') .. '/mason/packages/bash-debug-adapter/bash-debug-adapter',
+        command = vim.fn.stdpath('data') ..
+          '/mason/packages/bash-debug-adapter/bash-debug-adapter',
         name = 'bashdb',
       }
 
@@ -44,8 +51,10 @@ return {
           request = 'launch',
           name = 'Launch file',
           showDebugOutput = true,
-          pathBashdb = vim.fn.stdpath('data') .. '/mason/packages/bash-debug-adapter/extension/bashdb_dir/bashdb',
-          pathBashdbLib = vim.fn.stdpath('data') .. '/mason/packages/bash-debug-adapter/extension/bashdb_dir',
+          pathBashdb = vim.fn.stdpath('data') ..
+            '/mason/packages/bash-debug-adapter/extension/bashdb_dir/bashdb',
+          pathBashdbLib = vim.fn.stdpath('data') ..
+            '/mason/packages/bash-debug-adapter/extension/bashdb_dir',
           trace = true,
           file = '${file}',
           program = '${file}',
@@ -59,6 +68,8 @@ return {
           terminalKind = 'integrated',
         },
       }
+
+      require('nvim-dap-virtual-text').setup()
     end,
   },
   {
@@ -68,13 +79,13 @@ return {
       'mfussenegger/nvim-dap',
       'rcarriga/nvim-dap-ui',
     },
-    config = function()
+    config = function ()
       local mason_registry = require('mason-registry')
       local debugpy = mason_registry.get_package('debugpy')
       local install_path = debugpy:get_install_path() .. '/venv/bin/python'
       require('dap-python').setup(install_path)
 
-      vim.keymap.set('n', '<leader>dpr', function()
+      vim.keymap.set('n', '<leader>dpr', function ()
         require('dap-python').test_method()
       end, { desc = 'Python: test method' })
     end,
@@ -86,7 +97,7 @@ return {
       'mfussenegger/nvim-dap',
       'rcarriga/nvim-dap-ui',
     },
-    config = function()
+    config = function ()
       local mason_registry = require('mason-registry')
       local delve = mason_registry.get_package('delve')
       local install_path = delve:get_install_path() .. '/dlv'
@@ -97,11 +108,11 @@ return {
       }
       require('dap-go').setup(opts)
 
-      vim.keymap.set('n', '<leader>dgt', function()
+      vim.keymap.set('n', '<leader>dgt', function ()
         require('dap-go').debug_test()
       end, { desc = 'Go: debug test' })
 
-      vim.keymap.set('n', '<leader>dgl', function()
+      vim.keymap.set('n', '<leader>dgl', function ()
         require('dap-go').debug_last()
       end, { desc = 'Go: debug last test' })
     end,
@@ -114,7 +125,7 @@ return {
   {
     'jbyuki/one-small-step-for-vimkind',
     event = 'VeryLazy',
-    config = function()
+    config = function ()
       local dap = require('dap')
       dap.configurations.lua = {
         {
@@ -124,8 +135,13 @@ return {
         },
       }
 
-      dap.adapters.nlua = function(callback, config)
-        callback({ type = 'server', host = config.host or '127.0.0.1', port = config.port or 8086 })
+      dap.adapters.nlua = function (callback, config)
+        callback({
+          type = 'server',
+          host = config.host or '127.0.0.1',
+          port =
+            config.port or 8086
+        })
       end
     end,
   },
